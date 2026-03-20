366d660c6a8a4150e83d4651a9ec916ea483327e
srikanth-52 <nampellisrikanth52@gmail.com>
2026-03-20 14:47:07 +0530

    Update predictor.py

--- AI_Job_Role_Detector/utils/predictor.py
+++ AI_Job_Role_Detector/utils/predictor.py
@@ -1,5 +1,13 @@
+import os
 import pickle
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+model_path = os.path.join(BASE_DIR, "model", "model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
 # Load saved model
 model = pickle.load(open("model/model.pkl", "rb"))
 vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
